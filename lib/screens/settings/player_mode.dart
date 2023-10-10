9f952d52fc47896d3155a3ab183a7fe3896f8225
bolaji Adeladan <bolaji.adeladan@gmail.com>
2023-10-10 17:52:31 +0100

    more skeleton

---
+++ lib/screens/settings/player_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlayerMode extends StatelessWidget {
+  const PlayerMode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
