b5b0093176fc677184c31a05b0a42fa69e8e298b
Paul Sola-Eniolawun <70896150+Musecreatives@users.noreply.github.com>
2023-10-11 12:57:29 +0100

    authentication UI in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'screens/auth/sign_up.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: SignUpScreen(),
     );
   }
 }
---
+++ lib/widgets/app_custom_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+
+class AppButton extends StatelessWidget {
+  const AppButton(
+      {super.key,
+      required this.buttonText,
+      required this.onPressed,
+      this.buttonWidth = double.infinity});
+  final String buttonText;
+  final Function() onPressed;
+  final double buttonWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: buttonWidth,
+      height: 50,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(
+            AppTheme.primaryColor,
+          ),
+          elevation: MaterialStateProperty.all<double>(0),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100),
+            ),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 20,
+            color: AppTheme.whiteColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
